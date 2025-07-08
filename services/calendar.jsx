68647ef113138cc144c82afaa07fbedfcd2adbeb
Jackson Pontes <jackson_d2@outlook.com>
2025-07-08 01:36:49 -0300

    feat: new calendar design

--- services/calendar.jsx
+++ services/calendar.jsx
@@ -1,11 +1,11 @@
 import api from './api'
 
-export const getSubjectsBySemester = async (semester) => {
+export const getAllSubjects = async () => {
   try {
-    const response = await api.get(`/semester/${semester}`);
+    const response = await api.get('/subject');
     return response.data;
   } catch (error) {
     console.error("Erro ao buscar disciplinas:", error);
     return [];
   }
-}
+};
